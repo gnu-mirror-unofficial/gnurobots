e06eaab1a7b31cbe1526a3df1c1d57f609e93e5d
Bradley Smith <brad@brad-smith.co.uk>
2008-01-21 17:47:21 +0000

    Correct fsf address in copyright.
    Signed-off-by: Bradley Smith <brad@brad-smith.co.uk>

--- src/userinterface.c
+++ src/userinterface.c
@@ -1,23 +1,23 @@
 /* $Id: userinterface.c,v 1.7 2004/10/21 19:24:30 zeenix Exp $ */
-
-/* GNU Robots game engine.  This is the User Interface module */
-
-/* Copyright (C) 1998 Jim Hall, jhall1@isd.net */
-
 /*
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * GNU Robots game engine.  This is the User Interface module
+ *
+ * Copyright (C) 1998 Jim Hall <jhall1@isd.net>
+ * Copyright (C) 2008 Bradley Smith <brad@brad-smith.co.uk>
+ *
+ * GNU Robots is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNU Robots is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Robots; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include <glib.h>
@@ -28,7 +28,7 @@
 /*enum
 {
   ARG_0,
-  ARG_MAP 
+  ARG_MAP
 };*/
 
 GType _user_interface_type = 0;
@@ -38,7 +38,8 @@ static void user_interface_base_init (UserInterfaceClass * klass);
 GType
 user_interface_get_type (void)
 {
-  if (!_user_interface_type) {
+  if (!_user_interface_type)
+  {
     static const GTypeInfo interface_info = {
       sizeof (UserInterfaceClass),
       (GBaseInitFunc) user_interface_base_init,
