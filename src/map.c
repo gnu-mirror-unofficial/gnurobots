639b27375b8767f699c543fc0ea844f47a0f9f7c
Bradley Smith <brad@brad-smith.co.uk>
2008-01-21 17:42:51 +0000

    Corrected fsf address in copyright.
    Signed-off-by: Bradley Smith <brad@brad-smith.co.uk>

--- src/map.c
+++ src/map.c
@@ -1,27 +1,27 @@
-/* GNU Robots. */
-
-/* Copyright (C) 1998 Jim Hall, jhall1@isd.net */
-
 /*
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * GNU Robots.
+ *
+ * Copyright (C) 1998 Jim Hall <jhall1@isd.net>
+ * Copyright (C) 2008 Bradley Smith <brad@brad-smith.co.uk>
+ *
+ * GNU Robots is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNU Robots is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Robots; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-
-#include <unistd.h>             /* for getopt */
+#include <unistd.h>     /* for getopt */
 #include <stdio.h>
-#include <string.h>             /* for strdup */
+#include <string.h>     /* for strdup */
 
 #include <glib.h>
 
